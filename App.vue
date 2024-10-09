450a1d3a409b70309415446f71500238ad068311
Orlando Caullan <orlicaullan@gmail.com>
2024-10-09 16:11:52 -0300

    Se hace la entrega de taller 3

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>Consumo de APIs</h1>
+    <BotonesDeApis />
+  </div>
+</template>
+
+<script>
+import BotonesDeApis from './components/BotonesDeApis.vue';
+
+export default {
+  components: {
+    BotonesDeApis,
+  },
+};
+</script>
